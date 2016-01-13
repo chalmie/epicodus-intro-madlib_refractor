2bd715b822e95708a5145b592f17003ddedae0ae
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-13 14:15:54 -0800

    add function to loop that inserts strings from array into the madlib spans

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   // build array
-  var blanks = ["input","input","input","input","input","input"];
+  var blanks = ["input1","input2","input3","input4","input5","input6"];
 
   // test loop to confirm that blanks is constructed right
   blanks.forEach(function(blank) {
-    alert(blank);
+    var arrayWord = blank;
+    // alert(arrayWord);
+    // puts the text into the corrent span
+    $('#' + blank).text(blank);
   });
 
   // forEach loop that will apply array value to correct span
