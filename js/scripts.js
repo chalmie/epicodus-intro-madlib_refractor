94addafae81cc8322006ac54ec90477de4920dd8
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-13 14:24:11 -0800

    add js functionality places text from answers array into the correct spans

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-  // build array
+  // array for name of span ids
   var blanks = ["input1","input2","input3","input4","input5","input6"];
+  // array for answers
+  var answers = ["hotdog", "pizza", "jazz", "jumping", "swim", "freedom"]
 
-  // test loop to confirm that blanks is constructed right
+  // loops for populating the madlib
   blanks.forEach(function(blank) {
-    var arrayWord = blank;
-    // alert(arrayWord);
-    // puts the text into the corrent span
-    $('#' + blank).text(blank);
+    // puts the text into the correct span
+    // this code works by taking the last character of a string in the blanks array with .slice(-1), changing it to an interger, then using that integer to refer to the correct location in answers array
+    $('#' + blank).text(answers[parseInt(blank.slice(-1))]);
   });
 
   // forEach loop that will apply array value to correct span
