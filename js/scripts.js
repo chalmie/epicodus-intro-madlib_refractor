700954b00f8ccd4e7f893aa4da2ff43d984f0f17
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-13 14:08:05 -0800

    add basic loop to move through array, alerts array content to confirm function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   // build array
+  var blanks = ["input","input","input","input","input","input"];
+
+  // test loop to confirm that blanks is constructed right
+  blanks.forEach(function(blank) {
+    alert(blank);
+  });
 
   // forEach loop that will apply array value to correct span
+
 
 });
